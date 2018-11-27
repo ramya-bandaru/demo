d9329eba6d1c4429a5be6a53ab8c44d320b344b3
Ramyasri <ramyasri.a19@gmail.com>
2018-11-27 19:35:24 +0530

    Add user accounts with devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # Required for devise . Remember to change locolhost : 3000 to actual applcation
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
